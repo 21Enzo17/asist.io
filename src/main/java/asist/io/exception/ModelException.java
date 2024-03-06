068bd8fa98edd3f9b81c048167d9b285e0642e5f
21Enzo17 <129308114+21Enzo17@users.noreply.github.com>
2024-03-06 03:48:47 -0300

    Pequeño fix en el pom y en modelException

--- src/main/java/asist/io/exception/ModelException.java
+++ src/main/java/asist/io/exception/ModelException.java
@@ -1,14 +1,7 @@
 package asist.io.exception;
 
-<<<<<<< HEAD
 public class ModelException extends RuntimeException {
     public ModelException(String mensaje) {
 		super(mensaje);
 	}
-=======
-public class ModelException extends Exception {
-    public ModelException(String message) {
-        super(message);
-    }
->>>>>>> 942b73736804b168eed8ad7ea63de4d1101bed9a
 }
