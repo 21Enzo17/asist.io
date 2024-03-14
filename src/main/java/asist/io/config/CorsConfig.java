512420c6a1996fcc22c8f53692dcb3732fda0d9a
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-03-13 23:54:42 -0300

    chore: remove unused imports

--- src/main/java/asist/io/config/CorsConfig.java
+++ src/main/java/asist/io/config/CorsConfig.java
@@ -1,14 +1,9 @@
 package asist.io.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.UrlBasedViewResolverRegistration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
