89300636dc50e5b8135902894709ea3c9ed481e1
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-05-07 00:40:46 -0300

    feat: implementar exception filters en Curso

--- src/main/java/asist/io/service/ICursoService.java
+++ src/main/java/asist/io/service/ICursoService.java
@@ -5,6 +5,7 @@ import asist.io.dto.cursoDTO.CursoPatchDTO;
 import asist.io.dto.cursoDTO.CursoPostDTO;
 import asist.io.entity.Curso;
 import asist.io.exception.ModelException;
+import asist.io.exception.filters.HttpException;
 
 import java.util.List;
 
@@ -13,33 +14,33 @@ public interface ICursoService {
      * Registra un curso en la base de datos
      * @param curso Curso a registrar
      * @return Curso registrado
-     * @throws ModelException Si el curso es nulo
+     * @throws HttpException Si el curso es nulo
      */
-    public CursoGetDTO registrarCurso(CursoPostDTO curso) throws ModelException;
+    public CursoGetDTO registrarCurso(CursoPostDTO curso) throws HttpException;
 
     /**
      * Elimina un curso de la base de datos
      * @param id Id del curso a eliminar
      * @return true si el curso fue eliminado, false si no existe el curso
-     * @throws ModelException Si el id es nulo o vacío
+     * @throws HttpException Si el id es nulo o vacío
      */
-    public boolean eliminarCurso(String id) throws ModelException;
+    public boolean eliminarCurso(String id) throws HttpException;
 
     /**
      * Actualiza un curso en la base de datos
      * @param curso curso con datos actualizados
      * @return Curso actualizado
-     * @throws ModelException Si el curso es nulo, el id es nulo o vacío o el curso no existe
+     * @throws HttpException Si el curso es nulo, el id es nulo o vacío o el curso no existe
      */
-    public CursoGetDTO actualizarCurso(CursoPatchDTO curso) throws ModelException;
+    public CursoGetDTO actualizarCurso(CursoPatchDTO curso) throws HttpException;
 
     /**
      * Obtiene un curso por su id
      * @param id Id del curso
      * @return Curso si existe, null si no existe
-     * @throws ModelException Si el id es nulo o vacío
+     * @throws HttpException Si el id es nulo o vacío
      */
-    public CursoGetDTO obtenerCursoPorId(String id) throws ModelException;
+    public CursoGetDTO obtenerCursoPorId(String id) throws HttpException;
 
     /**
      * Obtiene un curso ENTITY por su id
@@ -53,32 +54,33 @@ public interface ICursoService {
      * Obtiene un curso por su código de asistencia
      * @param codigoAsistencia Código de asistencia del curso
      * @return Curso si existe en formato CursoGetDTO
-     * @throws ModelException Si el código de asistencia es nulo o vacío, o el curso no existe
+     * @throws HttpException Si el código de asistencia es nulo o vacío, o el curso no existe
      */
-    public CursoGetDTO obtenerCursoPorCodigoAsistencia(String codigoAsistencia) throws ModelException;
+    public CursoGetDTO obtenerCursoPorCodigoAsistencia(String codigoAsistencia) throws HttpException;
 
 
     /**
      * Obtiene los cursos que pertenecen a un usuario
      * @param id Id del usuario
      * @return Lista de cursos que pertenecen al usuario
-     * @throws ModelException Si el id es nulo o vacío
+     * @throws HttpException Si el id es nulo o vacío
      */
-    public List<CursoGetDTO> obtenerCursosPorIdUsuario(String id) throws ModelException;
+    public List<CursoGetDTO> obtenerCursosPorIdUsuario(String id) throws HttpException;
 
     /**
      * Obtiene los cursos según una palabra clave que coincida con el nombre
      * @param termino Palabra clave para buscar cursos
      * @param usuarioId Id del usuario
      * @return Lista de cursos que contienen la palabra clave
+     * @throws HttpException Si el término es nulo o vacío
      */
-    public List<CursoGetDTO> obtenerCursosPorTerminoYUsuario(String termino,String usuarioId) throws ModelException;
+    public List<CursoGetDTO> obtenerCursosPorTerminoYUsuario(String termino,String usuarioId) throws HttpException;
 
     /**
      * Encuentra un curso por su código de asistencia
      * @param codigoAsistencia Código de asistencia del curso
      * @return Entidad Curso si existe
-     * @throws ModelException Si el código de asistencia es nulo o vacío
+     * @throws HttpException Si el código de asistencia es nulo o vacío
      */
     public Curso obtenerCursoEntityPorCodigoAsistencia(String codigoAsistencia);
 
@@ -95,6 +97,6 @@ public interface ICursoService {
      * @throws ModelException Si no se pudo generar el código de asistencia
      * debido a multiples intentos fallidos
      */
-    public String generarCodigoAsistencia() throws ModelException;
+    public String generarCodigoAsistencia() throws HttpException;
 
 }
