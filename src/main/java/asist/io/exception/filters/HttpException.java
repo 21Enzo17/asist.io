2cd4144af324c1ec4089cea48e088f76b55bb00c
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-05-06 22:07:29 -0300

    fix: basic exception filters

--- src/main/java/asist/io/exception/filters/HttpException.java
+++ src/main/java/asist/io/exception/filters/HttpException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public abstract class HttpException extends RuntimeException {
     public HttpException(String cause, HttpStatus status) {
-        super(cause + " - " + status);
+        super(cause);
     }
 }
--- src/main/java/asist/io/handler/FilterExceptionHandler.java
+++ src/main/java/asist/io/handler/FilterExceptionHandler.java
@@ -6,30 +6,58 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+
 @ControllerAdvice
 public class FilterExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity handleNotFoundException(NotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        Map<String, Object> body = Map.of(
+                "message", ex.getMessage(),
+                "status", "404 Not Found"
+        );
+
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+        Map<String, Object> body = Map.of(
+                "message", ex.getMessage(),
+                "status", "401 Unauthorized"
+        );
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity handleBadRequestException(BadRequestException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        Map<String, Object> body = Map.of(
+                "message", ex.getMessage(),
+                "status", "400 Bad Request"
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity handleForbiddenException(ForbiddenException ex) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+        Map<String, Object> body = Map.of(
+                "message", ex.getMessage(),
+                "status", "403 Forbidden"
+        );
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(body);
     }
 
     @ExceptionHandler(InternalServerErrorException.class)
     public ResponseEntity handleInternalServerErrorException(InternalServerErrorException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        Map<String, Object> body = Map.of(
+                "message", ex.getMessage(),
+                "status", "500 Internal Server Error"
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
     }
 }
