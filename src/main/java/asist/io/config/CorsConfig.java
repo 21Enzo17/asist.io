d9d65de9ace51e070a8e2c7030f676b72b6ecea6
21Enzo17 <129308114+21Enzo17@users.noreply.github.com>
2024-08-15 18:29:02 -0300

    Fix de cors

--- src/main/java/asist/io/config/CorsConfig.java
+++ src/main/java/asist/io/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("https://grand-panda-d208e4.netlify.app").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
