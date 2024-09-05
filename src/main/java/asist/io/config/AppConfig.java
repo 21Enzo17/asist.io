f9fd4388e6cb6522b4ea64020ecb3ce87e5d81d0
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-09-05 01:48:59 -0300

    feature: GetUser decorator
    Añadir un nuevo decorador para facilitar la obtención de los datos del usuario atraves del JWT

--- src/main/java/asist/io/config/AppConfig.java
+++ src/main/java/asist/io/config/AppConfig.java
@@ -1,14 +1,27 @@
 package asist.io.config;
 
+import asist.io.decorators.resolvers.GetUserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
-public class CorsConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private GetUserArgumentResolver getUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(getUserArgumentResolver);
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
