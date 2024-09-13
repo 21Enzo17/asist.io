c20c71e32f3cebe743a3e41fa285cdfaceb2e66f
21Enzo17 <129308114+21Enzo17@users.noreply.github.com>
2024-09-13 17:37:48 -0300

    Fix2 en la autorizacion de la obtencion de un codigo de asistencia

--- src/main/java/asist/io/auth/SecurityConfig.java
+++ src/main/java/asist/io/auth/SecurityConfig.java
@@ -140,7 +140,7 @@ public class SecurityConfig  {
                 auth.requestMatchers("/api/v1/usuario/cambiar-contrasena/**").permitAll();
                 auth.requestMatchers("/api/v1/usuario/reenviar-correo-confirmacion").permitAll();
                 auth.requestMatchers("/api/v1/asistencias/registrar").permitAll();
-                auth.requestMatchers("/api/v1/cursos/codigo-asistencia/**");
+                auth.requestMatchers("/api/v1/cursos/codigo-asistencia/**").permitAll();
                 auth.anyRequest().authenticated();
             })
             .sessionManagement(session ->{
