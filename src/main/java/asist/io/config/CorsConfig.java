ab485fa0380b49c8c11e53bd4cd655d0a14466bf
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-09-13 16:47:14 -0300

    add: cors config

--- src/main/java/asist/io/config/CorsConfig.java
+++ src/main/java/asist/io/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package asist.io.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,13 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig {
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("*").allowedHeaders("*");
             }
         };
     }
